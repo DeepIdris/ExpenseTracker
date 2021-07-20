de63aa5ea1d97ef5078a5ef0a42971d61bb3940e
Idris Oyebode <oyebodeidris@gmail.com>
2021-07-20 02:55:30 +0100

    End of Project

---
+++ ExpenseTracker/ExpenseTracker/Controllers/ExpenseController.cs
@@ -0,0 +1,105 @@
+﻿using ExpenseTracker.Data;
+using ExpenseTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ExpenseController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Expense> itemList = _db.Expenses;
+            return View(itemList);
+        }
+
+        //GET-Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST-Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Expense item)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Expenses.Add(item);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+
+        //POST-Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int? id)
+        {
+            var expense = _db.Expenses.Find(id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+            _db.Expenses.Remove(expense);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        //GET-Delete
+        public IActionResult DeleteGet(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var expense = _db.Expenses.Find(id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+            return View(expense);
+        }
+
+        //GET Update
+        public IActionResult Update(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var expense = _db.Expenses.Find(id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+            return View(expense);
+        }
+
+
+        //POST Update
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Expense obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Expenses.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+    }
+}
---
+++ ExpenseTracker/ExpenseTracker/Controllers/ItemController.cs
@@ -0,0 +1,40 @@
+﻿using ExpenseTracker.Data;
+using ExpenseTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Item> itemList = _db.Items;
+            return View(itemList);
+        }
+
+        //GET-Create
+        public IActionResult Create()
+        {    
+            return View();
+        }
+
+        //POST-Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Item item)
+        {
+            _db.Items.Add(item);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
